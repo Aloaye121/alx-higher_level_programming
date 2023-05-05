c1ebeae206f2847a0c7b0724491297f01d10f338
abdul razak <muhammadjamiuabdulrazak@gmail.com>
2023-05-06 00:28:07 +0100

    correction

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 number=98
-name="Battery street"
-print(f"{number} {name}")
+number=int(number)
+print(f"{number} Battery street")
 
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-Float =3.75
-print(f"{Float}")
+number = 3.16
+print(f"Float: {number}")
